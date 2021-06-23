f9a5dad0f6e233dcca8d143438137f9f3727d7fc
chris formey <chrisformey@yahoo.com>
2021-06-23 10:52:23 -0400

    add folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 
 function App() {
-  return 'hello';
+  return <div>
+   <Folder name="Desktop" />
+   <Folder name= "Applications" />
+   <Folder />
+</div>
 
 }
+
+const Folder = (props) => {
+  return <h1>{props.name}</h1>
+};
 
 export default App;
